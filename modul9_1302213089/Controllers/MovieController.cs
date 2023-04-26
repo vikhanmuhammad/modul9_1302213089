38c48ca1572ede200b879f33fda915e2e9e0b594
vikhanmuharram <vikhanmuharram@student.telkomuniversity.ac.id>
2023-04-26 16:59:42 +0700

    menambahkan API movies

---
+++ modul9_1302213089/Controllers/MovieController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using System;
+using test3;
+using modul9_1302213089;
+
+namespace test3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class MovieController : Controller
+    {
+        private static List<Movie> movies = new List<Movie>
+        {
+            new Movie(
+                "The Shawshank Redemption",
+                "Frank Darabont",
+                new List<string>(new[] { "Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler" }),
+                "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion"
+            ),
+            new Movie
+            (
+                "The Godfather",
+                "Francis Ford Coppola",
+                new List<string>(new[]{ "Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton" }),
+                "The aging patriarch of an organized crime dynasty in postwar " +
+                "New York City transfers control of his clandestine empire to his reluctant youngest son."
+            ),
+            new Movie
+            (
+                "The Dark Knight",
+                " Christopher Nolan",
+                new List<string>( new[] {"Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine" }),
+                "When the menace known as the Joker wreaks havoc and chaos " +
+                "on the people of Gotham, Batman must accept one of the greatest psychological " +
+                "and physical tests of his ability to fight injustice."
+            )
+        };
+
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return movies;
+        }
+
+        [HttpGet("{id}")]
+        public Movie Get(int id)
+        {
+            return movies[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Movie value)
+        {
+            movies.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Movie value)
+        {
+            movies[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            movies.RemoveAt(id);
+        }
+    }
+}
---
+++ modul9_1302213089/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace modul9_1302213089
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        //public Movie() { }
+        public Movie(string Title, string Director, List<string> Stars, string Description)
+        {
+            this.Title = Title;
+            this.Director = Director;
+            this.Stars = Stars;
+            this.Description = Description;
+        }
+    }
+}
